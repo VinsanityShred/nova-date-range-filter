86c4fb0427b391b1819b611da4a94ddbfd033e77
dennislindsey <denniseblindsey@gmail.com>
2021-02-03 16:47:49 -0800

    Update DateRangeFilter.php

--- src/DateRangeFilter.php
+++ src/DateRangeFilter.php
@@ -22,4 +22,9 @@ abstract class DateRangeFilter extends Filter
     {
         return $this->withMeta(['placeholder' => $placeholder]);
     }
+
+    public function options(Request $request)
+    {
+        return [];
+    }
 }
